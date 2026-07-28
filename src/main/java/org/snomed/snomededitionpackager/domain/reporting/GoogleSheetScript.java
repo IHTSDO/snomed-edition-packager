7213593d77aa75fca2d65a0812a72ebc114bc9ab
Darryn McGaw <dmcg@ihtsdo.org>
2026-07-28 15:10:39 +0100

    PIP-1065 Fix compilation errors

--- src/main/java/org/snomed/snomededitionpackager/domain/reporting/GoogleSheetScript.java
+++ src/main/java/org/snomed/snomededitionpackager/domain/reporting/GoogleSheetScript.java
@@ -1,12 +1,13 @@
 package org.snomed.snomededitionpackager.domain.reporting;
 
+import org.snomed.otf.Environment;
 import org.snomed.otf.scheduler.domain.JobRun;
 import org.snomed.otf.script.Script;
 import org.snomed.otf.script.dao.DataBroker;
 
 public class GoogleSheetScript extends Script {
 	private String reportName;
-	private String reportEnvironment;
+	private Environment reportEnvironment;
 
 	@Override
 	public boolean isOffline() {
@@ -28,17 +29,12 @@ public class GoogleSheetScript extends Script {
 	}
 
 	@Override
-	public String detectReleaseBranch() {
-		return null;
-	}
-
-	@Override
-	public String getEnv() {
+	public Environment getEnv() {
 		return reportEnvironment;
 	}
 
 	public void setReportEnvironment(String reportEnvironment) {
-		this.reportEnvironment = reportEnvironment;
+		this.reportEnvironment = Environment.fromString(reportEnvironment);
 	}
 
 	@Override
